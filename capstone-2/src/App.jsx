3ffdec7c3c8a82c0af8cbc232983650233ec0ec8
Aditya Bhardwaj <Adityasir10@gmail.com>
2025-05-02 23:16:49 +0530

    commit-0

--- capstone-2/src/App.jsx
+++ capstone-2/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='bg-red-500 text-white text-9xl text-center'>Hello Guneet</h1>
+      <h1 className='bg-red-500 text-white text-9xl text-center'>
+        Hello Aditya !
+      </h1>
     </div>
   )
 }
