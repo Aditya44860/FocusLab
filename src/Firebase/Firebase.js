9fdd42e9c3a0a30e5ba0bcb7e1b11affb70d038b
Aditya Bhardwaj <Adityasir10@gmail.com>
2025-07-05 13:43:13 +0530

    production ready

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore"
 import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ7wf-23Ol39LT-xKR6kkgbtrOnpnk9i0",
-  authDomain: "focuslab-dd77b.firebaseapp.com",
-  databaseURL: "https://focuslab-dd77b-default-rtdb.firebaseio.com",
-  projectId: "focuslab-dd77b",
-  storageBucket: "focuslab-dd77b.firebasestorage.app",
-  messagingSenderId: "539748841505",
-  appId: "1:539748841505:web:46cd243e78a7e177a34b60",
-  measurementId: "G-H938FW3VFD"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
